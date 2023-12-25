96683b4be0d594876335e6cb714fd6f18092e391
Mateus Schverz <matefs8569@gmail.com>
2023-12-25 15:24:59 -0300

    Create transaction-savepoint-example.sql

---
+++ TRANSACTIONS/transaction-savepoint-example.sql
@@ -0,0 +1,31 @@
+
+
+create table CONTA(
+ nro_conta int primary key,
+ nome_titular varchar(30) not null,
+ saldo real default 0
+);
+
+insert into conta(nro_conta, nome_titular, saldo) 
+ values(1, 'Marta', 25400.00);
+insert into conta(nro_conta, nome_titular, saldo) 
+ values(2, 'Lucas', 1281.34); 
+
+select * From conta order by nro_conta;
+
+begin; 
+    update conta set saldo = saldo - 100 where nro_conta = 1 ; -- only this line will work
+
+    savepoint debitoEmConta;
+    update conta set saldo = saldo - 100 where nro_conta = 2;
+
+    rollback to debitoEmConta;
+
+commit;
+
+
+select * From conta order by nro_conta;
+
+
+
+
