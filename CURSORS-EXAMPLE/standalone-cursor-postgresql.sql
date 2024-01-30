1f4dbe254c4de49b748b8b6c5c9dbfd4857fb906
Mateus Schverz <matefs8569@gmail.com>
2024-01-30 11:14:15 -0300

    fix: removed comments

--- CURSORS-EXAMPLE/standalone-cursor-postgresql.sql
+++ CURSORS-EXAMPLE/standalone-cursor-postgresql.sql
@@ -1,29 +1,22 @@
--- Create table
+
 CREATE TABLE EMPLOYEE (
   empId INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   dept TEXT NOT NULL
 );
 
--- Insert data
+ 
 INSERT INTO EMPLOYEE VALUES (0001, 'Clark', 'Sales');
 INSERT INTO EMPLOYEE VALUES (0002, 'Dave', 'Accounting');
 INSERT INTO EMPLOYEE VALUES (0003, 'Ava', 'Sales');
 
--- Fetch data using cursor
--- Start a transaction block
 BEGIN;
 
--- Declare a cursor
 DECLARE my_cursor CURSOR FOR SELECT name, dept FROM EMPLOYEE WHERE name IS NOT NULL;
 
--- Fetch and process the data
 FETCH FROM my_cursor;
--- You can add additional processing logic here
 
--- Close the cursor
 CLOSE my_cursor;
 
--- Commit the transaction
 COMMIT;
 
